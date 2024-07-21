5d39d68d2b13df1fe614ba233f50a8133f75da91
DelNats <152296143+DelNats@users.noreply.github.com>
2024-07-21 18:41:27 +0100

    created a dataframe to present results from fruityvice.

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,9 +46,11 @@ if ingredient_lit:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'+ name_on_order+'!', icon="✅", )
 
+# Importing info from fruityvice.com
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 #######################################
 

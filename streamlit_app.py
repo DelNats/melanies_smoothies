cebeea8db8d83573b465ee16ccd7ff9e0e73d4b4
DelNats <152296143+DelNats@users.noreply.github.com>
2024-07-22 10:04:17 +0100

    Update streamlit_app.py
    removed stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # convert the Snowflake DF to a Pandas DF in order to use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredient_lit =  st.multiselect(
     "Choose up to 5 ingredients",
@@ -67,41 +67,3 @@ if ingredient_lit:
 
 
 
-#######################################
-
-# option = st.selectbox(
-#     "Select your favourite fruit?",
-#     ("Apple", "Passion Fruit", "Blueberries"))
-
-# st.write("Your selected Fruit is:", option)
-
-# # Get the current credentials
-# session = get_active_session()
-
-# # Use an interactive slider to get user input
-# hifives_val = st.slider(
-#     "Number of high-fives in Q3",
-#     min_value=0,
-#     max_value=90,
-#     value=60,
-#     help="Use this to enter the number of high-fives you gave in Q3",
-# )
-
-# #  Create an example dataframe
-# #  Note: this is just some dummy data, but you can easily connect to your Snowflake data
-# #  It is also possible to query data using raw SQL using session.sql() e.g. session.sql("select * from table")
-# created_dataframe = session.create_dataframe(
-#     [[50, 25, "Q1"], [20, 35, "Q2"], [hifives_val, 30, "Q3"]],
-#     schema=["HIGH_FIVES", "FIST_BUMPS", "QUARTER"],
-# )
-
-# # Execute the query and convert it into a Pandas dataframe
-# queried_data = created_dataframe.to_pandas()
-
-# # Create a simple bar chart
-# # See docs.streamlit.io for more types of charts
-# st.subheader("Number of high-fives")
-# st.bar_chart(data=queried_data, x="QUARTER", y="HIGH_FIVES")
-
-# st.subheader("Underlying data")
-# st.dataframe(queried_data, use_container_width=True)

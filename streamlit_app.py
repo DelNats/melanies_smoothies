e8c03b91c60cb42f235301a63cbf3819c5206048
DelNats <152296143+DelNats@users.noreply.github.com>
2024-07-22 09:47:44 +0100

    Update streamlit_app.py
    added missing column col('FRUIT_NAME')

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,13 +20,13 @@ name_on_order = st.text_input('Name on Smoothie')
 cnx=st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
 # convert the Snowflake DF to a Pandas DF in order to use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredient_lit =  st.multiselect(
     "Choose up to 5 ingredients",
